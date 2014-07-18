2a708f4a08a7991a69cf7f6f794d89567bf2d869
Jake Scott <jscott@addthis.com>
2014-07-18 09:53:40 -0400

    second commit

--- public/js/test.js
+++ public/js/test.js
@@ -2,11 +2,12 @@ var fn = function () {
 	document.write('<div>a</div>');
 	document.write('<div>b</div>');
 	document.write('<div>c</div>');
-	document.write('<div>hello</div>');
+	document.write('<div><b>goodbye</b></div>');
 	document.write('<div>world</div>!');
 	document.write('<div>d</div>');
 	document.write('<div>e</div>');
 	document.write('<div>f</div>');
+	document.write('trololol');
 };
 
 fn();
