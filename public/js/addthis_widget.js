e423fa20fba9746d161b5cc88a361d91d2ddd77a
Jake Scott <jscott@addthis.com>
2014-07-18 09:58:12 -0400

    update sample rates

--- public/js/addthis_widget.js
+++ public/js/addthis_widget.js
@@ -42,10 +42,10 @@ var _atd = 'www-local.addthis.com/',
         ver : 250,
         loc : 0,
         enote : '', // pre-filled email note
-        cwait : 1000, // menu close wait onmouseout in milliseconds
+        cwait : 500, // menu close wait onmouseout in milliseconds
         bamp : 1, // -- clickback sample rate (-1=off, 1=on)
         camp : 1, // -- classification sample rate (-1=off, 1=on)
-        csmp : 0.03, // -- clipboard copy sample rate
+        csmp : 0.0001, // -- clipboard copy sample rate
         damp : 1, // -- lo.json sample
         famp : 1.0, // -- file path referer sample rate (-1=off, 1=on)
         pamp : 1, // -- personalization sample rate (-1=off, 1=on)
