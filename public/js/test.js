581e5ea8817e31a54bcf9917680adc1e73ddde09
Jake Scott <jscott@addthis.com>
2014-07-18 09:50:16 -0400

    ..

--- public/js/test.js
+++ public/js/test.js
@@ -2,6 +2,8 @@ var fn = function () {
 	console.log('a');
 	console.log('b');
 	console.log('c');
+	console.log('hello');
+	console.log('world!');
 	console.log('d');
 	console.log('e');
 	console.log('f');
